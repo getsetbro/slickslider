e38a79bce8dabcd65470178ae1fa1807c711379e
Seth Broweleit <hello[@]getsetbro[.]com>
2014-12-17 12:42:51 -0600

    conditional

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,71 +4,121 @@
  */
 
 (function(window, $, undefined) {
-
+    var Int = {};
     var Carousel = {
         //init
+        playerTemplate: function(videopID){
+          return '<div id="bcp_' + videopID + '">' +
+          '<div style="display:none"></div>'+
+          '<object class="BrightcoveExperience" id="bcv_' + videopID + '">'+
+          '<param name="bgcolor" value="#000000" />'+
+          '<param name="width" value="530" />'+
+          '<param name="height" value="300" />'+
+          '<param name="isVid" value="true" />'+
+          '<param name="isUI" value="true" />'+
+          '<param name="htmlFallback" value="true" />'+
+          '<param name="dynamicStreaming" value="true" />'+
+          '<param name="playerKey" value="AQ~~,AAABZYdAfHk~,rfwhGZGAOzpg9is3SWrPSaKzW3y1dPmF" />'+
+          //'<param name="playerID" value="3266798266001" />'+ //replaced by the playerKey param
+          '<param name="@videoPlayer" value="' + videopID + '"; />'+
+          '<param name="includeAPI" value="true" />'+
+          //'<param name="templateLoadHandler" value="Carousel.onTemplateLoad" />'+
+          '<param name="templateReadyHandler" value="Carousel.onTemplateReady" />'+
+          '</object>'+
+          '</div>';
+        },
+        slickCarousel:function(){
+          Carousel.prop_carousel = $('#prop_carousel').slick({
+            slidesToShow: 1,
+            centerMode: true,
+            variableWidth: true,
+            autoplay:false,
+            autoplaySpeed:6000
+            //speed:300
+          });
+        },
+        slickLightbox:function(){
+          Carousel.prop_lightbox = $('#prop_lightbox').slick({
+            //speed:300
+          });
+        },
+        bindImgClicks: function(){
+          Carousel.imgClicksBound = true;
+          $('#prop_carousel').find('img').click(function(){
+            Carousel.prop_carousel.slickPause();
+            var carouselcurrent = $(this).parent().attr('index');
+            $('.Lightbox').removeClass('Lightbox--hidden');
+            Carousel.prop_lightbox.slickGoTo(carouselcurrent);
+          });
+        },
         init: function() {
-              $('#scroller').slick({
-                slidesToShow: 1,
-                centerMode: true,
-                variableWidth: true
-              });
-          var playerTemplate = function(videopID){
-            return ''+
-            '<div class="playerHide" id="bcp_' + videopID + '">' +
-            '<object class="BrightcoveExperience" id="bcv_' + videopID + '">'+
-            '<param name="bgcolor" value="#000000" />'+
-            '<param name="width" value="530" />'+
-            '<param name="height" value="300" />'+
-            '<param name="isVid" value="true" />'+
-            '<param name="isUI" value="true" />'+
-            '<param name="dynamicStreaming" value="true" />'+
-            '<param name="playerKey" value="AQ~~,AAABZYdAfHk~,rfwhGZGAOzpg9is3SWrPSaKzW3y1dPmF" />'+
-            '<param name="playerID" value="3266798266001" />'+
-            '<param name="@videoPlayer" value="' + videopID + '"; />'+
-            '<param name="includeAPI" value="true" />'+
-            //'<param name="templateLoadHandler" value="Carousel.onTemplateLoad" />'+
-            '<param name="templateReadyHandler" value="Carousel.onTemplateReady" />'+
-            '</object>'+
-            '<div class="playerClose" onClick="Carousel.hideAndStop(' + videopID + ');">Close</div>'+
-            '</div>';
-          };
+          var propImagesWrap = $('#prop_images');
+          var propImages = propImagesWrap.find('>div');
+          if(propImages.length < 2){
+            //do nothing
+            return false;
+          }
+          //continue to create carousel
+          propImages.appendTo('#prop_carousel,#prop_lightbox');
 
-          $('[data-bcvid]').each( function(index, item){
-              $('#players').append( playerTemplate(item.dataset.bcvid) );
+          Carousel.slickLightbox();
+          Carousel.slickCarousel();
+          //videos
+          var prop_videos = $('#prop_videos');
+          var prop_videos_data = prop_videos.data('bcvid');
+          Carousel.prop_videos_arr = JSON.parse("[" + prop_videos_data + "]");
+          //if no videos then do not continue
+          if( !prop_videos || !prop_videos_data || !Carousel.prop_videos_arr.length ){
+            //bind image clicks now that would have been bound after videos
+            Carousel.bindImgClicks();
+            return false;
+          }
+
+          $.each(Carousel.prop_videos_arr, function(i){
+              Carousel.prop_lightbox.slickAdd( Carousel.playerTemplate( Carousel.prop_videos_arr[i] ), true );
           });
 
-          //brightcove.createExperiences();
+          brightcove.createExperiences();
 
         },
         player:null,
         APIModules:null,
         videoPlayer:{},
-        //onTemplateLoad: function(experienceID) {
-          //console.log("EVENT: onTemplateLoad");
-        //},
+        playersAdded:[],
+        addAtIndex:0,
+        // onTemplateLoad: function(experienceID) {
+        //   console.log("EVENT: onTemplateLoad");
+        // },
         onTemplateReady: function(event) {
-//        console.log("EVENT.onTemplateReady");
-          Carousel.player = brightcove.api.getExperience(event.target.experience.id);
-          Carousel.videoPlayer[event.target.experience.id] = Carousel.player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
-          Carousel.videoPlayer[event.target.experience.id].getCurrentVideo(function(video){
-            $("#scroller").slickAdd("<div><img src='" + video.videoStillURL + "' onClick='Carousel.showAndLoad(" + video.id + ");'/></div>", true);
+          //console.log("EVENT.onTemplateReady");
+          var experienceID = event.target.experience.id;
+          Carousel.player = brightcove.api.getExperience(experienceID);
+          Carousel.videoPlayer[experienceID] = Carousel.player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+          Carousel.videoPlayer[experienceID].getCurrentVideo(function(video){
+
+            if( Carousel.playersAdded.indexOf(video.id) === -1 ){
+              Carousel.prop_carousel.slickAdd("<div id='s_"+video.id+"'><img src='" + video.videoStillURL + "'/></div>", Carousel.addAtIndex, true );
+              Carousel.playersAdded.push(video.id);
+              Carousel.addAtIndex++;
+            }
+
+            if( Carousel.playersAdded.length === Carousel.prop_videos_arr.length ){
+            Carousel.prop_carousel.slickPlay();
+
+              Carousel.bindImgClicks();
+            }
 
           });
         },
-        showAndLoad: function(current) {
-            //console.log("EVENT.onClick " + current);
-            //console.log(videoData.videos[current].shortDescription);
-            // reveal the lightbox
-            $("#bcp_"+current).attr("class", "playerShow");
-            // load and play the selected video
-            Carousel.videoPlayer['bcv_' + current].loadVideoByID(current);
-        },
         hideAndStop: function(active) {
-              console.log(active);
-            Carousel.videoPlayer['bcv_' + active].pause();
+            $.each(Carousel.videoPlayer, function(key,val){
+              Carousel.videoPlayer[key].pause();
+            });
             // hide the lightbox
-            $("#bcp_"+active).attr("class", "playerHide");
+            //$("#prop_carousel").attr("class", "playerHide");
+            Carousel.prop_carousel.slickPlay();
+            $('.Lightbox').addClass('Lightbox--hidden');
+
         }
     };
 
